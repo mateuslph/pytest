07a466d731e3226fe72272210eb279b6cf1c705b
mateuslph <mlptche@gmail.com>
2023-02-15 19:47:07 -0300

    test retorna Carvalho

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -10,3 +10,12 @@ class TestClass:
         resultado = funcionario_teste.idade() # When - Acao
 
         assert resultado == esperado # Then - Desfecho
+
+    def test_quando_nome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = ' Lucas Carvalho ' # Given
+        esperado = 'Carvalho'
+
+        lucas = Funcionario(entrada, '2000', 1111)
+        resultado = lucas.sobrenome() # When
+
+        assert resultado == esperado # Then
